577153a14e458f169439fc008ce0966e061f19f2
mohitsavaliya123 <89637908+mohitsavaliya123@users.noreply.github.com>
2022-01-04 10:25:15 +0530

    Create parent_child_7.js
    Manage too many Props with Parent/Child Component

---
+++ parent_child_7.js
@@ -0,0 +1,12 @@
+render () {
+let { addressInfo, currentUserPos } = this.props;
+let { addHouseNumber,
+    addStreetName,
+    addStreetDirection,
+    addCity,
+    addState,
+    addZip,
+    addLat,
+    addLon } = addressInfo;
+return ( ... );
+}
