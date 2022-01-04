e1cbd8048293216cb8b44481cf0af812894f9179
mohitsavaliya123 <89637908+mohitsavaliya123@users.noreply.github.com>
2022-01-04 10:24:07 +0530

    Create parent_child_5.js
    Manage too many Props with Parent/Child Component

---
+++ parent_child_5.js
@@ -0,0 +1,11 @@
+let addressList = [];
+addressList.push({
+ addHouseNumber: "12344",
+ addStreetName: "Street Road",
+ addStreetDirection: "N",
+ addCity: "My City",
+ addState: "ST",
+ addZip: "12346",
+ addLat: "019782356834",
+ addLon: "02384575757"
+});
